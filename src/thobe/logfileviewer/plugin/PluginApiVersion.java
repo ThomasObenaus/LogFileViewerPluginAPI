7657d8586edd518a60212bdbdb52617c228c29a3
Thomas Obenaus <obenaus.thomas@gmail.com>
2015-04-11 19:08:51 +0200

    .DOC .RELEASE version 3.2.0

--- src/thobe/logfileviewer/plugin/PluginApiVersion.java
+++ src/thobe/logfileviewer/plugin/PluginApiVersion.java
@@ -22,17 +22,17 @@ public class PluginApiVersion implements IPluginApiVersion
 	/**
 	 * Increase for incompatible changes
 	 */
-	private static final int	MAJOR_VERSION	= 3;
+	private static final int MAJOR_VERSION = 3;
 
 	/**
 	 * Increase for compatible changes
 	 */
-	private static final int	MINOR_VERSION	= 3;
+	private static final int MINOR_VERSION = 2;
 
 	/**
 	 * Increase for bugfixes
 	 */
-	private static final int	BUGFIX_VERSION	= 0;
+	private static final int BUGFIX_VERSION = 0;
 
 	private int					majorVersion;
 	private int					minorVersion;
